f793c147f60eb76e502198c62b3d5b526e294d16
HisyamRefatha <refatha76@gmail.com>
2025-07-21 19:04:25 +0700

    pushhh

--- presentation/belyuk.web/src/App.tsx
+++ presentation/belyuk.web/src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { HouseIcon, InboxIcon, SparklesIcon, ZapIcon } from "lucide-react"
+import { ThemeProvider } from "@/components/theme/theme-provider"
+import { ModeToggle } from "@/components/theme/mode-toggle"
+import { Button } from "@/components/ui/button"
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <ThemeProvider>
+            <header className="fixed top-2 md:top-5 w-full px-2 md:px-5 z-50">
+                <div className="border border-border/80 rounded-xl bg-card/80 backdrop-blur-md h-12 md:h-16 flex justify-between items-center gap-2 px-4 shadow-lg/2">
+                    <div className="flex-1 flex items-center">
+                        
+                    </div>
+                    <div className="grow flex justify-center">
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                    </div>
+                    <div className="flex-1 flex justify-end items-center gap-4">
+                        <Button size="sm" className="text-sm rounded-lg">
+                            Share
+                        </Button>
+                        <ModeToggle/>
+                    </div>
+                </div>
+            </header>
+        </ThemeProvider>
+    )
 }
 
 export default App
